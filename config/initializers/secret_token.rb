44f0313fc6c522803e3287081660c8a2e7d718b6
Leonardo Maciel <leonardomaciel.lmf@gmail.com>
2012-02-07 10:24:09 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oficina::Application.config.secret_token = 'd1cd8e28d2630f41a4b3b569fe48963f76dff391f21b9942e58145cc1e6a095ff5d489ce5163d4badbe5479f9f8b6b03adbc8241d444ced996807cbd0f6da26e'
