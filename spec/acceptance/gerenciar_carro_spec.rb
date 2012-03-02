8af6cff8d405cca89d678814acacfc9f9b5437d0
Leonardo Maciel <leonardomaciel.lmf@gmail.com>
2012-03-02 10:12:56 -0300

    Alterar carro

--- spec/acceptance/gerenciar_carro_spec.rb
+++ spec/acceptance/gerenciar_carro_spec.rb
@@ -29,5 +29,38 @@ feature 'gerenciar carro' do
     page.should have_content 'Ano: ano text'
     page.should have_content 'Nome: cliente text'
   end
+
+   scenario 'alterar carro' do #, :javascript => true do
+
+    cliente = FactoryGirl.create(:cliente,:nome => 'cliente text')
+    carro = FactoryGirl.create(:carro, :cliente => cliente)
+
+    visit edit_carro_path(carro)
+
+    preencher_e_verificar_carro
+
+  end
+
+  
+
+ 
+  def preencher_e_verificar_carro
+
+    fill_in 'Modelo', :with => 'modelo text'
+    fill_in 'Placa', :with => 'placa text'
+    fill_in 'Ano', :with => 'ano text'
+    select 'cliente text', :on => 'Nome'
+        
+    click_button 'Salvar'
+   
+   
+   page.should have_content 'Modelo: modelo text'
+    page.should have_content 'Placa: placa text'
+    page.should have_content 'Ano: ano text'
+    page.should have_content 'Nome: cliente text'
+
+  end
+
+
 end
 
