5da5db601a75a68f51350aa389beb9fc123649f5
Leonardo Maciel <leonardomaciel.lmf@gmail.com>
2012-03-04 18:31:53 -0300

    Alterar mecanico

--- spec/acceptance/gerenciar_mecanico_spec.rb
+++ spec/acceptance/gerenciar_mecanico_spec.rb
@@ -14,18 +14,36 @@ feature 'gerenciar mecanico' do
   scenario 'incluir mecanico' do
   visit new_cliente_path
 
-    fill_in 'Nome', :with => 'cliente text'
+    fill_in 'Nome', :with => 'mecanico text'
     fill_in 'Telefone', :with => 'telefone text'
 
         	
     click_button 'Salvar'
 
-    page.should have_content 'Nome: cliente text'
+    page.should have_content 'Nome: mecanico text'
     page.should have_content 'Telefone: telefone text'
 
 
   end
 
+  scenario 'alterar mecanico' do #, :javascript => true  do
+
+    mecanico = FactoryGirl.create(:mecanico)
+
+    visit edit_mecanico_path(mecanico)
+
+    fill_in 'Nome', :with => 'mecanico alterado'
+    fill_in 'Telefone', :with => 'telefone alterado'
+    fill_in 'Especialidade', :with => 'especialidade alterado'
+
+        
+    click_button 'Salvar'
+
+    page.should have_content 'Nome: mecanico alterado'
+    page.should have_content 'Telefone: telefone alterado'
+    page.should have_content 'Especialidade: especialidade alterado'
 
 
+
+end
 end
