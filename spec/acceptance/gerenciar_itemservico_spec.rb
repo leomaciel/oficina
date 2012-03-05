39a1c6d9c765740ed8f4eb04fbc940d06cf12f54
Leonardo Maciel <leonardomaciel.lmf@gmail.com>
2012-03-04 21:30:30 -0300

    Alterar Itemservico

--- spec/acceptance/gerenciar_itemservico_spec.rb
+++ spec/acceptance/gerenciar_itemservico_spec.rb
@@ -34,4 +34,35 @@ feature 'gerenciar itemservico' do
   end
 
 
+  scenario 'alterar itemservico' do #, :javascript => true do
+    
+    servico = FactoryGirl.create(:servico,:data => 'data text')
+    mecanico = FactoryGirl.create(:mecanico,:nome => 'mecanico text')
+    itemservico = FactoryGirl.create(:itemservico, :mecanico => mecanico, :servico =>servico)
+
+    visit edit_itemservico_path(itemservico)
+
+    preencher_e_verificar_itemservico
+
+  end
+
+
+
+  def preencher_e_verificar_itemservico
+
+   fill_in 'Nomeitem', :with => 'nomeitem text'
+    fill_in 'Precoitem', :with => 'precoitem text'
+    select 'mecanico text', :on => 'Nome'
+    select 'data text', :on => 'Data'        
+
+    click_button 'Salvar'
+
+    page.should have_content 'Nomeitem: nomeitem text'
+    page.should have_content 'Precoitem: precoitem text'
+    page.should have_content 'Nome: mecanico text'
+    page.should have_content 'Data: data text'
+
+  end
+
+
 end
